64b6725b1f835c7687be8b7c263188ce855d81d1
Daniel Scarbrough <35717456+Zarbof@users.noreply.github.com>
2022-03-05 14:44:42 -0800

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Edit <code>src/App.js</code> I WISH IT WOULD SNOW!!!!!
+         Edit <code>src/App.js</code> I WISH IT WOULD RAIN!!!!!
         </p>
         <a
           className="App-link"
