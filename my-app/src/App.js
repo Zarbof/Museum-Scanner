582c9310ff20f4fd35d9314c55874bf5809a3b43
Kate S <57074302+ktschh@users.noreply.github.com>
2022-03-05 15:04:57 -0800

    Inserted scanner code
    Using qrscan components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import QRScan from 'qrscan'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Edit <code>src/App.js</code> I WISH IT WOULD RAIN!!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { value: '', watching: false }
+        this.onFind = this.onFind.bind(this)
+    }
+
+    onFind(value) {
+        this.setState({ value, watching: false })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>QRScan Demo</h1>
+                {this.state.watching
+                    ? (
+                        <QRScan onFind={this.onFind} />
+                    )
+                    : (
+                        <React.Fragment>
+                            <button onClick={() => this.setState({ watching: true })}>Scan</button>
+                            <h4>value: {this.state.value}</h4>
+                        </React.Fragment>
+                    )
+                }
+            </React.Fragment>
+        )
+    }
 }
 
 export default App;
