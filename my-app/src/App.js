abaa632e72b56444f50d54aca0dde87fc88912ee
Kate S <57074302+ktschh@users.noreply.github.com>
2022-03-05 15:14:21 -0800

    RETRY scanner code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,6 @@
 import React, { Component } from 'react'
 import QRScan from 'qrscan'
 
-<<<<<<< HEAD
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Edit <code>src/App.js</code> I WISH THERE WOULD BE A THUNDERSTORM!!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 class App extends Component {
 
     constructor(props) {
@@ -52,7 +31,6 @@ class App extends Component {
             </React.Fragment>
         )
     }
->>>>>>> 582c9310ff20f4fd35d9314c55874bf5809a3b43
 }
 
 export default App;
