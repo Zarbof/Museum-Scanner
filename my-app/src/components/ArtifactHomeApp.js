5562c3bda18c9b111467e312ffbc3533083f4b42
Kate S <ktschh@outlook.com>
2022-04-04 17:11:59 -0700

    Initial attempt at artifact layout

--- my-app/src/components/ArtifactHomeApp.js
+++ my-app/src/components/ArtifactHomeApp.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Button, Alert, Nav, Container, Navbar, NavDropdown } from 'react-bootstrap'
+import { Tabs } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function ArtifactHomeApp() {
   return(
     <div>
 
-      <h2>Artifact Body!!</h2>
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+        <Tab eventKey="media" title="Media">
+          <p>Here is where media will go</p>
+        </Tab>
+        <Tab eventKey="description" title="Description">
+          <p>Here is where description text will go</p>
+        </Tab>
+      </Tabs>
       
     </div>
     
