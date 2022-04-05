df20b349ead3520e8bf8dccdb5dfba363061aab3
Kate S <ktschh@outlook.com>
2022-04-04 17:31:46 -0700

    Added Listgroup and image to media tab

--- my-app/src/components/ArtifactHomeApp.js
+++ my-app/src/components/ArtifactHomeApp.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Tabs , Tab } from 'react-bootstrap'
+import { Tabs , Tab , ListGroup } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function ArtifactHomeApp() {
   return(
     <div class="text-center">
 
+
       <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
         <Tab eventKey="media" title="Media">
-          <p>Here is where media will go</p>
+          <ListGroup>
+          <ListGroup.Item>
+          <Image src="https://greatinspire.com/wp-content/uploads/2016/09/The-Beauty-Of-Small-Things-Photography-4.jpg"></Image>
+            flowers :)
+          </ListGroup.Item>
+          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
         </Tab>
         <Tab eventKey="description" title="Description">
           <p>Here is where description text will go</p>
