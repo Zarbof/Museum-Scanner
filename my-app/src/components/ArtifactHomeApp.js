e2f4dfa17f3bc467a7509f9be1993109257d2e73
Kate S <ktschh@outlook.com>
2022-04-04 17:37:11 -0700

    Added import for image

--- my-app/src/components/ArtifactHomeApp.js
+++ my-app/src/components/ArtifactHomeApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs , Tab , ListGroup } from 'react-bootstrap'
+import { Tabs , Tab , ListGroup , Image } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function ArtifactHomeApp() {
