b4837a8568ae4f967e1749e6307b14eba33b968a
Angel Herrera <56905303+Angelhf@users.noreply.github.com>
2022-04-04 17:04:31 -0700

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,12 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Alert, Nav, Container, Navbar } from 'react-bootstrap'
+import { Nav, Container, Navbar } from 'react-bootstrap'
 
 import ArtifactHomeApp from './components/ArtifactHomeApp';
 import PlantBookApp from './components/PlantBookApp'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-
-// class App extends Component {
-//     // handleClick = () => {
-//     //     console.log('clicked');
-//     // }
-//     // handleClick = (e) => {
-//     //     e.preventDefault();
-//     //     console.log('The link was clicked.');
-//     //   };
-//     handleClick() {
-//         window.location.assign('http://lelooska.pugetsound.edu/phpApp/museumLogin.php');
-//     }
-//     render() {
-//         return (
-//             <Button onClick = {this.handleClick.bind(this)}>CLick me</Button>
-//           );
-          
-        
-//     }
-// }
     function App() {
         return (
         <div className="wrapper">
