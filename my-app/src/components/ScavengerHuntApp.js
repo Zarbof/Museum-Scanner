7395676fe3947361e7bc63bed12d38f62b911bc6
lleary <61063972+lleary@users.noreply.github.com>
2022-04-15 13:37:15 -0700

    undid thing

--- my-app/src/components/ScavengerHuntApp.js
+++ my-app/src/components/ScavengerHuntApp.js
@@ -1,120 +1,33 @@
 import React from 'react';
-import { Accordion, Card, CardGroup } from 'react-bootstrap'
-import { ReactSession } from 'react-client-session';
+import { Tabs , Tab , ListGroup , Image } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-
-
-export default function ScavengerHuntApp(props) {
-  const plantType = props.plantType
-  const username = ReactSession.get("username");
-
-  var rows = [];
-  for (var i = 0; i < 5; i++) {
-    rows.push(<Accordion.Item eventKey={i} key={i}>
-    <Accordion.Header>{username}</Accordion.Header>
-    <Accordion.Body>
-    <CardGroup>
-      <Card>
-        <Card.Img variant="top" src="https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in to
-            additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to additional
-            content.{' '}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in to
-            additional content. This card has even longer content than the first to
-            show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-    </CardGroup>
-
-    </Accordion.Body>
-  </Accordion.Item>);
-  } 
-
+export default function ScavengerHuntApp() {
   return(
-    <div>
-      <Accordion defaultActiveKey="0">
-        {/* <Accordion.Item eventKey="0">
-          <Accordion.Header>Plant Type</Accordion.Header>
-          <Accordion.Body>
-          <CardGroup>
-            <Card>
-              <Card.Img variant="top" src="https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src="https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This card has supporting text below as a natural lead-in to additional
-                  content.{' '}
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src="https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/What-Are-Plants.jpg" />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This card has even longer content than the first to
-                  show that equal height action.
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-          </CardGroup>
+    <div class="text-center">
 
-          </Accordion.Body>
-        </Accordion.Item> */}
-        { rows }
-      </Accordion>
+
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+        <Tab eventKey="media" title="Media">
+          <ListGroup>
+          <ListGroup.Item>
+            <Image src="https://greatinspire.com/wp-content/uploads/2016/09/The-Beauty-Of-Small-Things-Photography-4.jpg"></Image>
+            <p>flowers :)</p>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <p>please code this soon</p>
+          </ListGroup.Item>
+          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
+        </Tab>
+        <Tab eventKey="description" title="Description">
+          <p>Here is where description text will go</p>
+        </Tab>
+      </Tabs>
+      
     </div>
+    
   );
 }
