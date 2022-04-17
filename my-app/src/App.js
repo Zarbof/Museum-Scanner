a9b94d7c20bd45a091f1607cf77f38a60d7485ea
Angel Herrera <56905303+Angelhf@users.noreply.github.com>
2022-04-17 14:49:09 -0700

    commi

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,29 +13,45 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
         ReactSession.setStoreType("localStorage");
         ReactSession.set("username", "TestUsername");
 
+        if(ReactSession.get("token") == "dogs") {
+            return (
+                <div className="wrapper">
+                    <BrowserRouter>
+                        <Navbar bg="light" expand="lg">
+                            <Container>
+                                <Navbar.Brand href="#home">Lelooska</Navbar.Brand>
+                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav className="me-auto">
+                                    <Nav.Link href="plantbook">Plant Book</Nav.Link>
+                                    <Nav.Link href="http://lelooska.pugetsound.edu/phpApp/museumLogin.php">Admin Login</Nav.Link>
+                                </Nav>
+                                </Navbar.Collapse>
+                            </Container>
+                        </Navbar>
+                    <Routes>
+                        <Route path="home" element={<ArtifactHomeApp />} />
+                        <Route path="plantbook" element={<PlantBookApp plantType={plantType} />} />
+        
+                    </Routes>
+                    </BrowserRouter>
+                </div>
+                );
+        }
         return (
-        <div className="wrapper">
-            <BrowserRouter>
-                <Navbar bg="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand href="#home">Lelooska</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="plantbook">Plant Book</Nav.Link>
-                            <Nav.Link href="http://lelooska.org">Lelooska Foundation main website</Nav.Link>
-                            <Nav.Link href="http://lelooska.org/donate/"> Donate</Nav.Link>
-                            <Nav.Link href="http://lelooska.pugetsound.edu/phpApp/museumLogin.php">Admin Login</Nav.Link>
-                        </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            <Routes>
-                <Route path="home" element={<ArtifactHomeApp />} />
-                <Route path="plantbook" element={<PlantBookApp plantType={plantType} />} />
-            </Routes>
-            </BrowserRouter>
-        </div>
+            <div className="wrapper">
+                <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1">Token</InputGroup.Text>
+                    <FormControl
+                        placeholder="Username"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                    />
+                    <Button type="text">Submit</Button>
+                </InputGroup>
+                </div>
         );
     }
 export default App;
+            
+    
