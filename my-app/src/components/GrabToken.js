2bf1d1495671df39b421ab06d069e02177f2e467
Daniel Scarbrough <35717456+Zarbof@users.noreply.github.com>
2022-04-17 15:36:53 -0700

    s

--- my-app/src/components/GrabToken.js
+++ my-app/src/components/GrabToken.js
@@ -8,7 +8,7 @@ const AccessDataBase = (props) => {
     const presplit = result.replace(/[^,.:}'0-9a-zA-Z ]/g, "")
     const split = presplit.split("}");
     const split1 = split[0].split(",");
-    // const split2 = split[1].split(",");
+
     
   
 
@@ -33,35 +33,17 @@ const AccessDataBase = (props) => {
     return (
         <div className="App">
             <form
-                action="http://lelooska.pugetsound.edu/phpApp/getData.php"
+                action="http://lelooska.pugetsound.edu/phpApp/getAccessCode.php"
                 method="post"
                 onSubmit={(event) => handleSumbit(event)}
             >
-                <label htmlFor="id">Artifact ID: </label>
-                <input
-                    type="text"
-                    id="id"
-                    name="id"
-                    value={id}
-                    onChange={(event) => handleChange(event)}
-                />
+                <label htmlFor="id">Token: </label>
                 <br />
-                <button type="submit">Submit</button>
+                <button type="submit">Retrieve Token</button>
             </form>
             <h1>{result}</h1>
             <h1>{length}</h1>
-            
-            <h1>{split.length-1}</h1>
-            <h1>{split[0]}</h1>
-            <h1>{split[1]}</h1>
-            <h1>{split[2]}</h1>
-            <h1>{split[3]}</h1>
-            <h1>{split[4]}</h1>
-            <h1>{split[5]}</h1>
-            <h1>{split[6]}</h1>
-            <h1>{split[7]}</h1>
-            <h1>{split[8]}</h1>
-            {/* <h1>{split2[2]}</h1> */}
+
 
             
            
