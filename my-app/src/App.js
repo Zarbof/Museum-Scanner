413fe1882d5b9ead3ffc22e05abc57f8e7e1e988
Angel Herrera <56905303+Angelhf@users.noreply.github.com>
2022-04-21 12:52:12 -0700

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -32,7 +32,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
             type: "GET",
             success: function(data) {
               console.log('success')
-              console.log(data.results);
+              console.log(data[0]);
             }.bind(this),
             error: function(xhr, status, err) {
               console.log('error')
