63565328ec2dc6300e69be6e072345b05516308b
Angel Herrera <56905303+Angelhf@users.noreply.github.com>
2022-04-28 13:09:42 -0700

    comimt

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -39,6 +39,46 @@ import Cookies from 'universal-cookie';
                     console.log('The tokens are not the same')
                     ReactSession.set("token", dailyTokenString);
                     setDailyDBToken(dailyTokenString);
+                    return (
+                        <div className="wrapper" style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                            <Navbar bg="light" expand="lg">
+                                <Container>
+                                    <Navbar.Brand >Lelooska</Navbar.Brand>
+                                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                    <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className="me-auto">
+                                        <Nav.Link href="http://lelooska.pugetsound.edu/phpApp/museumLogin.php">Admin Login</Nav.Link>
+                                        <Nav.Link href="http://lelooska.org/">Lelooska Foundation Website</Nav.Link>
+                                    </Nav>
+                                    </Navbar.Collapse>
+                                </Container>
+                            </Navbar>
+            
+                            <Card >
+                                <Card.Body>
+                                    <Card.Title>Please enter the access token.</Card.Title>
+                                    <Card.Text>
+                                    <InputGroup className="mb-3">
+                                <InputGroup.Text id="basic-addon1">Token</InputGroup.Text>
+                                    <FormControl
+                                        onChange={ handleChange }
+                                        placeholder="Access Token"
+                                        aria-label="Access Token"
+                                        aria-describedby="basic-addon1"
+                                    />
+                                    <Button type="text" onClick={onFormSubmitSuccess}>Submit</Button>
+                                </InputGroup>
+                                    </Card.Text>
+                                </Card.Body>
+            
+                            </Card>
+                            
+                        </div>
+                    );
                   } 
                 }.bind(this),
                 error: function(xhr, status, err) {
