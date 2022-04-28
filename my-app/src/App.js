1f5565a120b010211fbc18d13f07c011f913a550
Angel Herrera <56905303+Angelhf@users.noreply.github.com>
2022-04-28 13:46:20 -0700

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,40 +17,13 @@ import Cookies from 'universal-cookie';
     function App() {
         const [userToken, setUserToken] = useState("");
         const [dailyDBToken, setDailyDBToken] = useState("");
-        // const [loggedIn, setLoggedIn] = useState("");
 
         ReactSession.setStoreType("localStorage");
         const cookies = new Cookies();
 
         const plantType = 'plant1';
 
-        useEffect(() => {
-            $.ajax({
-                url: 'http://lelooska.pugetsound.edu/phpApp/getAccessCode.php',
-                type: "GET",
-                success: function(data) {
-                  console.log('success with use effect')
-                  const myObject = JSON.parse(data);
-                  const dailyToken = JSON.stringify(myObject.tokenValue);
-                  const dailyTokenString = dailyToken.replaceAll('"', '');
-                
-                  const userTokenString = {userToken}.userToken;
-    
-                  if(dailyTokenString !== userTokenString) {
-                    console.log('The tokens are not the same')
-                    ReactSession.set("token", 'NOT VALID');
-                    cookies.set('token', 'NOT VALID', { path: '/' });
-                    cookies.set('loggedIn', 'False', { path: '/' });
-
-                    setDailyDBToken("NOT VALID");
-                    // setLoggedIn("False");
-                  } 
-                }.bind(this),
-                error: function(xhr, status, err) {
-                  console.log('error')
-                }.bind(this)
-              });
-        });
+        
 
         function onFormSubmitSuccess(e) {
             e.preventDefault();
@@ -65,21 +38,11 @@ import Cookies from 'universal-cookie';
               const dailyTokenString = dailyToken.replaceAll('"', '');
 
               const userTokenString = {userToken}.userToken;
-            
-              console.log("Strings after button input");
-              console.log("dailyTokenString ---");
-              console.log(dailyTokenString)
-              console.log("userTokenString ---");
-            
-              console.log(userTokenString)
 
               if(dailyTokenString === userTokenString) {
-                console.log('& the user token and db token are the same')
-                cookies.set('token', dailyTokenString, { path: '/' });
                 ReactSession.set("token", dailyTokenString);
                 setDailyDBToken(dailyTokenString);
-                cookies.set('loggedIn', 'True', { path: '/' });
-            } 
+              } 
             }.bind(this),
             error: function(xhr, status, err) {
               console.log('error')
@@ -91,59 +54,6 @@ import Cookies from 'universal-cookie';
             setUserToken(e.target.value);
             cookies.set('token', e.target.value, { path: '/' });
         }     
-
-        console.log("COOKIES ---");
-        console.log("ReactSession ---");
-        console.log(ReactSession.get("token"));
-        console.log("cookies ---");
-        console.log(cookies.get('token'));
-        console.log("dailyDBToken ---");
-        console.log({dailyDBToken}.dailyDBToken);
-        console.log("cookies loggedIn ---");
-        console.log(cookies.get('loggedIn'));
-
-        if(cookies.get('loggedIn') === "False") {
-            return (
-                <div className="wrapper" style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
-                    <Navbar bg="light" expand="lg">
-                        <Container>
-                            <Navbar.Brand >Lelooska</Navbar.Brand>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="http://lelooska.pugetsound.edu/phpApp/museumLogin.php">Admin Login</Nav.Link>
-                                <Nav.Link href="http://lelooska.org/">Lelooska Foundation Website</Nav.Link>
-                            </Nav>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
-    
-                    <Card >
-                        <Card.Body>
-                            <Card.Title>Please enter the access token.</Card.Title>
-                            <Card.Text>
-                            <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon1">Token</InputGroup.Text>
-                            <FormControl
-                                onChange={ handleChange }
-                                placeholder="Access Token"
-                                aria-label="Access Token"
-                                aria-describedby="basic-addon1"
-                            />
-                            <Button type="text" onClick={onFormSubmitSuccess}>Submit</Button>
-                        </InputGroup>
-                            </Card.Text>
-                        </Card.Body>
-    
-                    </Card>
-                    
-                </div>
-            );
-        }
 
         if(ReactSession.get("token") || cookies.get('token') === {dailyDBToken}.dailyDBToken) {
             return (
