0102cd9c8c0f61dd563598a6d061c9c7b13ab9ed
Kate S <ktschh@outlook.com>
2022-04-29 04:22:10 -0700

    styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -147,7 +147,7 @@ import Cookies from 'universal-cookie';
                             `}
                         </style>
                     </>
-                    <Navbar bg="light" expand="lg">
+                    <Navbar bg="light" expand="lg" className="color-nav">
                         <Container>
                             <Navbar.Brand href="home">Lelooska</Navbar.Brand>
                             <Navbar.Toggle aria-controls="basic-navbar-nav" />
