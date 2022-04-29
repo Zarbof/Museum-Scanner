0620cd440041bf50f9aaf1f2a8a2b581649de1ea
Daniel Scarbrough <35717456+Zarbof@users.noreply.github.com>
2022-04-29 01:21:18 -0700

    s

--- my-app/src/components/PlantBookApp.js
+++ my-app/src/components/PlantBookApp.js
@@ -151,7 +151,7 @@ console.log("cardsArray",cardsArray)
     rows.push(<Accordion.Item eventKey={i} key={i}>
       <Accordion.Header>{tempArray[i][1]}</Accordion.Header>
       <Accordion.Body>
-        {cardsArray}
+        {cardsArray[i]}
       </Accordion.Body>
     </Accordion.Item>);
   }
