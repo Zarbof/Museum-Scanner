a6d5a63aa8b925fe25480b13662ab3f44c8a8846
Daniel Scarbrough <35717456+Zarbof@users.noreply.github.com>
2022-04-29 02:17:44 -0700

    s

--- my-app/src/components/PlantBookApp.js
+++ my-app/src/components/PlantBookApp.js
@@ -133,7 +133,7 @@ export default function PlantBookApp(props) {
 
       console.log("finalArray", tempArrays);
       console.log("finalArray length", tempArrays.length);
-      console.log("arrayAccess test", tempArrays[0][5])
+      console.log("arrayAccess test", tempArrays[0][4])
       console.log("arrayAccess test", tempArrays[0][5][2])
 
 
