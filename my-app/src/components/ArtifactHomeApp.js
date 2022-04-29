bf2d738d9bfd581e2dccc2c9f475ab7ac7070f29
Kate S <ktschh@outlook.com>
2022-04-29 05:21:18 -0700

    button import

--- my-app/src/components/ArtifactHomeApp.js
+++ my-app/src/components/ArtifactHomeApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card , ListGroup, ListGroupItem , Nav } from 'react-bootstrap'
+import { Card , ListGroup, ListGroupItem , Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function ArtifactHomeApp() {
