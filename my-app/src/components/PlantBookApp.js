ef22810057e0539e06e32beecb8269bd51e98b04
Daniel Scarbrough <35717456+Zarbof@users.noreply.github.com>
2022-04-29 02:45:01 -0700

    s

--- my-app/src/components/PlantBookApp.js
+++ my-app/src/components/PlantBookApp.js
@@ -143,105 +143,105 @@ export default function PlantBookApp(props) {
 
 
       //create a card from each data point
-      //var mediaUrl = "http://lelooska.pugetsound.edu/phpApp/media/"
       for (var cardNum = 0; cardNum < tempArrays.length; cardNum++) {
-      //   // console.log("cardNum", tempArrays[cardNum]);
-      //   // let card = <CardFilePlants bigArray = {tempArrays[cardNum]}></CardFilePlants>;
-      //   // console.log("CARD",card);
-      //   var mediaNames = [];
-      //   var mediaDescriptions = [];
-      //   var mediaTypes = [];
-      //   const str1 = "a";
-      //   for (var i = 4; i < tempArrays.length; i++) {
-      //       var mediaNumber = tempArrays[i][2];
-      //       mediaNames[i - 1] = mediaNumber.split("a")[1];   //set back to pos 0 in assignment
-      //       mediaDescriptions[i - 1] = array[i][3];
-      //       if (array[i][1] == "photo") { //check if is set to photo and change to img for each element that is a photo
-      //           mediaTypes[i - 1] = "img";
-      //       }
-      //       else {
-      //           mediaTypes[i - 1] = array[i][1];
-      //       }
-      //       if (array[i][1] == "audio") { //check if is set to photo and change to img for each element that is a photo
-      //           console.log("audio before", mediaNames[i - 1])
-      //           if (!mediaNames[i - 1].endsWith("3")) {
-      //               mediaNames[i - 1] = mediaNames[i - 1].concat('', str1)
-      //               console.log("we have liftoff")
-      //           }
-      //           console.log(mediaNames[i - 1].concat('', str1))
-      //           console.log("audio after", mediaNames[i - 1])
+      var mediaUrl = "http://lelooska.pugetsound.edu/phpApp/media/"
+        // console.log("cardNum", tempArrays[cardNum]);
+        // let card = <CardFilePlants bigArray = {tempArrays[cardNum]}></CardFilePlants>;
+        // console.log("CARD",card);
+        var mediaNames = [];
+        var mediaDescriptions = [];
+        var mediaTypes = [];
+        const str1 = "a";
+        for (var i = 4; i < tempArrays.length; i++) {
+            var mediaNumber = tempArrays[i][2];
+            mediaNames[i - 1] = mediaNumber.split("a")[1];   //set back to pos 0 in assignment
+            mediaDescriptions[i - 1] = array[i][3];
+            if (array[i][1] == "photo") { //check if is set to photo and change to img for each element that is a photo
+                mediaTypes[i - 1] = "img";
+            }
+            else {
+                mediaTypes[i - 1] = array[i][1];
+            }
+            if (array[i][1] == "audio") { //check if is set to photo and change to img for each element that is a photo
+                console.log("audio before", mediaNames[i - 1])
+                if (!mediaNames[i - 1].endsWith("3")) {
+                    mediaNames[i - 1] = mediaNames[i - 1].concat('', str1)
+                    console.log("we have liftoff")
+                }
+                console.log(mediaNames[i - 1].concat('', str1))
+                console.log("audio after", mediaNames[i - 1])
                 
 
-      //       }
+            }
 
 
-      //   }
-      //   let card = <Card variant="outlined" style={{ textAlign: "center" }}>
-      //     <CardActionArea>
-      //       <CardContent>
-      //         <Typography gutterBottom variant="h5" component="div">
-      //           {tempArrays[0][1]}
-      //         </Typography>
-      //         <Typography variant="body2" color="text.secondary">
-      //           {tempArrays[0][2]}
-      //         </Typography>
-      //       </CardContent>
+        }
+        let card = <Card variant="outlined" style={{ textAlign: "center" }}>
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {tempArrays[0][1]}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {tempArrays[0][2]}
+              </Typography>
+            </CardContent>
 
-      //       <CardMedia
-      //         component={mediaTypes[0]}
-      //         controls
-      //         src={mediaUrl.concat('', mediaNames[0])}
-      //       />
-      //       <CardContent>
-      //         <Typography variant="body2" color="text.secondary">
-      //           {mediaDescriptions[0]}
-      //         </Typography>
-      //       </CardContent>
+            <CardMedia
+              component={mediaTypes[0]}
+              controls
+              src={mediaUrl.concat('', mediaNames[0])}
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {mediaDescriptions[0]}
+              </Typography>
+            </CardContent>
 
-      //       <CardMedia
-      //         component={mediaTypes[1]}
-      //         controls
-      //         src={mediaUrl.concat('', mediaNames[1])}
-      //       />
-      //       <CardContent>
-      //         <Typography variant="body2" color="text.secondary">
-      //           {mediaDescriptions[1]}
-      //         </Typography>
-      //       </CardContent>
-      //       <CardMedia
-      //         component={mediaTypes[2]}
-      //         controls
-      //         src={mediaUrl.concat('', mediaNames[2])}
-      //       />
-      //       <CardContent>
-      //         <Typography variant="body2" color="text.secondary">
-      //           {mediaDescriptions[2]}
-      //         </Typography>
-      //       </CardContent>
-      //       <CardMedia
-      //         component={mediaTypes[3]}
-      //         controls
-      //         src={mediaUrl.concat('', mediaNames[3])}
-      //       />
-      //       <CardContent>
-      //         <Typography variant="body2" color="text.secondary">
-      //           {mediaDescriptions[3]}
-      //         </Typography>
-      //       </CardContent>
-      //       <CardMedia
-      //         component={mediaTypes[4]}
-      //         controls
-      //         src={mediaUrl.concat('', mediaNames[4])}
-      //       />
-      //       <CardContent>
-      //         <Typography variant="body2" color="text.secondary">
-      //           {mediaDescriptions[4]}
-      //         </Typography>
-      //       </CardContent>
-      //     </CardActionArea>
-      //   </Card>
+            <CardMedia
+              component={mediaTypes[1]}
+              controls
+              src={mediaUrl.concat('', mediaNames[1])}
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {mediaDescriptions[1]}
+              </Typography>
+            </CardContent>
+            <CardMedia
+              component={mediaTypes[2]}
+              controls
+              src={mediaUrl.concat('', mediaNames[2])}
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {mediaDescriptions[2]}
+              </Typography>
+            </CardContent>
+            <CardMedia
+              component={mediaTypes[3]}
+              controls
+              src={mediaUrl.concat('', mediaNames[3])}
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {mediaDescriptions[3]}
+              </Typography>
+            </CardContent>
+            <CardMedia
+              component={mediaTypes[4]}
+              controls
+              src={mediaUrl.concat('', mediaNames[4])}
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {mediaDescriptions[4]}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-      // addCard(card);
+      addCard(card);
       }
 
 
