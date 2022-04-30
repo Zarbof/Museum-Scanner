49ad95a8ef464075de438a1796fd763c82776d77
Alysa Vermeulen <72112150+alysavermeulen@users.noreply.github.com>
2022-04-30 09:13:41 -0700

    Update museumLogin.php

--- phpApp/museumLogin.php
+++ phpApp/museumLogin.php
@@ -6,6 +6,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 	<head>
+		<link rel="icon" href="http://lelooska.org/wp-content/uploads/2013/01/salmon_transparent.png">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<link href='//fonts.googleapis.com/css?family=Forum|Open+Sans:600,regular,italic,700&amp;subset=latin' rel='stylesheet' type='text/css'>
 		<meta charset="UTF-8" />
