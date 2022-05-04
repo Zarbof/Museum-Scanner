be0a0c567ee763afe315343d248f78015e1e282f
Angel Herrera <56905303+Angelhf@users.noreply.github.com>
2022-05-04 15:53:09 -0700

    Added the token redirect page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -25,35 +25,35 @@ import Cookies from 'universal-cookie';
 
         const plantType = 'plant1';
 
-        // function onFormSubmitSuccess(e) {
-        //     e.preventDefault();
+        function onFormSubmitSuccess(e) {
+            e.preventDefault();
 
-        //   $.ajax({
-        //     url: 'http://lelooska.pugetsound.edu/phpApp/getAccessCode.php',
-        //     type: "GET",
-        //     success: function(data) {
-        //       console.log('success')
-        //       const myObject = JSON.parse(data);
-        //       const dailyToken = JSON.stringify(myObject.tokenValue);
-        //       const dailyTokenString = dailyToken.replaceAll('"', '');
+          $.ajax({
+            url: 'http://lelooska.pugetsound.edu/phpApp/getAccessCode.php',
+            type: "GET",
+            success: function(data) {
+              console.log('Sucessfully retrieved the db token during a button press.')
+              const myObject = JSON.parse(data);
+              const dailyToken = JSON.stringify(myObject.tokenValue);
+              const dailyTokenString = dailyToken.replaceAll('"', '');
+                console.log("The db token retrieved is: " + dailyTokenString);
+            //   const userTokenString = {userToken}.userToken;
 
-        //       const userTokenString = {userToken}.userToken;
+            //   if(dailyTokenString === userTokenString) {
+            //     ReactSession.set("token", dailyTokenString);
+            //     setDailyDBToken(dailyTokenString);
+            //   } 
+            }.bind(this),
+            error: function(xhr, status, err) {
+              console.log('error')
+            }.bind(this)
+          });
+        };
 
-        //       if(dailyTokenString === userTokenString) {
-        //         ReactSession.set("token", dailyTokenString);
-        //         setDailyDBToken(dailyTokenString);
-        //       } 
-        //     }.bind(this),
-        //     error: function(xhr, status, err) {
-        //       console.log('error')
-        //     }.bind(this)
-        //   });
-        // };
-
-        // function handleChange(e) {
-        //     setUserToken(e.target.value);
-        //     cookies.set('token', e.target.value, { path: '/' });
-        // }     
+        function handleChange(e) {
+            // setUserToken(e.target.value);
+            cookies.set('userToken', e.target.value);
+        }     
 
         // if(ReactSession.get("token") || cookies.get('token') === {dailyDBToken}.dailyDBToken) {
         //     return (
@@ -162,6 +162,54 @@ import Cookies from 'universal-cookie';
               });
         });
 
+        if(cookies.get('loggedIn') == false) {
+            return (
+                <div className="wrapper" style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                   <Container>
+                        <Row>
+                            {/* <Navbar bg="light" expand="lg">
+                                <Container>
+                                    <Navbar.Brand >Lelooska</Navbar.Brand>
+                                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                    <Navbar.Collapse id="basic-navbar-nav">
+                                        <Nav className="me-auto">
+                                            <Nav.Link href="http://lelooska.pugetsound.edu/phpApp/museumLogin.php">Admin Login</Nav.Link>
+                                            <Nav.Link href="http://lelooska.org/">Lelooska Foundation Website</Nav.Link>
+                                        </Nav>
+                                    </Navbar.Collapse>
+                                </Container>
+                            </Navbar> */}
+                        </Row>
+                        <Row className = "align-items-center">
+                            <Card >
+                                <Card.Body>
+                                    <Card.Title>Please enter the access token.</Card.Title>
+                                    <Card.Text>
+                                        <InputGroup className="mb-3">
+                                            <InputGroup.Text id="basic-addon1">Token</InputGroup.Text>
+                                            <FormControl
+                                                onChange={handleChange}
+                                                placeholder="Access Token"
+                                                aria-label="Access Token"
+                                                aria-describedby="basic-addon1"
+                                            />
+                                            <Button type="text" onClick={onFormSubmitSuccess}>Submit</Button>
+                                        </InputGroup>
+                                    </Card.Text>
+                                </Card.Body>
+    
+                            </Card>
+                        </Row>
+                    </Container>
+    
+                    
+                </div>
+            );  
+        }
         // with no access token
         return (
             <div className="wrapper">
